43a34941f5447e9b020af68a26e3245e76b79b33
jwar28 <javierguerra2203@gmail.com>
2024-05-24 18:14:07 -0500

    all pages centered

--- src/components/ui/hero/HeroComponent.jsx
+++ src/components/ui/hero/HeroComponent.jsx
@@ -12,16 +12,16 @@ export default function HeroComponent(props) {
           {props.description}
         </p>
         <div class='flex justify-center sm:justify-start'>
-          <button
-            type='button'
+          <a
+            href='/services'
             class='py-2.5 px-5 me-2 mb-2 text-sm font-medium focus:outline-none rounded-lg border focus:z-10 focus:ring-4 focus:ring-gray-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700'>
             Servicios
-          </button>
-          <button
-            type='button'
+          </a>
+          <a
+            href='/contact'
             class='py-2.5 px-5 me-2 mb-2 text-sm font-medium focus:outline-none rounded-lg border focus:z-10 focus:ring-4 focus:ring-gray-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700'>
             Contactanos
-          </button>
+          </a>
         </div>
       </div>
     </div>
