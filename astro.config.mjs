245fefaa30527f4d34b51661bcd995c60144ae8b
jwar28 <javierguerra2203@gmail.com>
2024-05-25 00:10:31 -0500

    sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), icon()],
+  site: 'https://techno-fix-five.vercel.app/',
+  integrations: [tailwind(), react(), icon(), sitemap()],
 });
